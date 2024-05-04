9457095e322005bbd1f90a4ecd969badd7314e26
Ishkhan05 <g.i.h.a.s@bk.ru>
2024-05-04 12:17:11 +0400

    MAcaron..

---
+++ src/Components/HOME/Content2/Content2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Content2() {
+  return (
+    <div className='Content2'>
+      <div className=''></div>
+      <div className=''></div>
+      <div className=''></div>
+      <div className=''></div>
+      <div className=''></div>
+      <div className=''></div>
+    </div>
+  )
+}

4d769da308fff6fae9e413c0f817c14cd6eea8ef
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-05-14 13:17:25 +0400

    fix modal

--- src/Constants/choose.js
+++ src/Constants/choose.js
@@ -1,44 +1,44 @@
 const initialChooseState = [
   {
-    id:1,
-    image:"/Images/Content2_images/Icon1.png",
-    title:"Ready-made kits",
-    desc:"Ready-made kits with a discount. You can choose a set for a suitable occasion.",
+    id: 1,
+    image: "./Images/Content2_images/Icon1.png",
+    title: "Ready-made kits",
+    desc: "Ready-made kits with a discount. You can choose a set for a suitable occasion.",
     bgImage: "Images/Content2_images/background1.png"
   },
   {
-    id:2,
-    image:"/Images/Content2_images/Icon2.png",
-    title:"Assemble your own set",
-    desc:"Choose the number of macaroons, and choose the flavors",
+    id: 2,
+    image: "Images/Content2_images/Icon2.png",
+    title: "Assemble your own set",
+    desc: "Choose the number of macaroons, and choose the flavors",
     bgImage: "Images/Content2_images/background2.png"
   },
   {
-    id:3,
-    image:"/Images/Content2_images/Icon3.png",
-    title:"A set with an individual seal",
-    desc:"Assemble a set of pasta with a unique design.",
+    id: 3,
+    image: "/Images/Content2_images/Icon3.png",
+    title: "A set with an individual seal",
+    desc: "Assemble a set of pasta with a unique design.",
     bgImage: "Images/Content2_images/background3.png"
   },
   {
-    id:4,
-    image:"/Images/Content2_images/Icon4.png",
-    title:"Wedding offers",
-    desc:"Delicate macaroni cakes with different flavors to decorate your wedding celebration",
+    id: 4,
+    image: "/Images/Content2_images/Icon4.png",
+    title: "Wedding offers",
+    desc: "Delicate macaroni cakes with different flavors to decorate your wedding celebration",
     bgImage: "Images/Content2_images/background4.png"
   },
   {
-    id:5,
-    image:"/Images/Content2_images/Icon5.png",
-    title:"Corporate gifts",
-    desc:"From $ 1 per piece. With a unique design. A pleasant compliment for colleagues and partners",
+    id: 5,
+    image: "/Images/Content2_images/Icon5.png",
+    title: "Corporate gifts",
+    desc: "From $ 1 per piece. With a unique design. A pleasant compliment for colleagues and partners",
     bgImage: "Images/Content2_images/background5.png"
   },
   {
-    id:6,
-    image:"/Images/Content2_images/Icon6.png",
-    title:"Wholesale supplies",
-    desc:"Offer for coffee shops, cafes, hotels, etc. Look at the terms of cooperation and reviews.",
+    id: 6,
+    image: "/Images/Content2_images/Icon6.png",
+    title: "Wholesale supplies",
+    desc: "Offer for coffee shops, cafes, hotels, etc. Look at the terms of cooperation and reviews.",
     bgImage: "Images/Content2_images/background6.png"
   },
 ]
